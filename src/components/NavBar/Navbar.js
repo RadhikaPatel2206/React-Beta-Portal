f2ab18d1a0ef90caf04c000f8c608064fd595bf7
Radhika Patel <64517071+RadhikaPatel2206@users.noreply.github.com>
2020-08-15 11:05:47 +0530

    Basic NavBar

--- src/components/NavBar/Navbar.js
+++ src/components/NavBar/Navbar.js
@@ -1,7 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { Button } from "@material-ui/core";
 
-const Navbar = () => {
-    return <div>Navbar</div>;
+// StyleSheet import
+import "./NavBar.css";
+
+const NavBar = () => {
+    // Render NavBar - ClassName using BEM
+    return (
+        <div className="navbar">
+            <div className="navbar__logo">Portal</div>
+            <div className="navbar__options">
+                <Link className="navbar__link" to="/">
+                    <Button className="navbar__button" variant="text">
+                        Login
+                    </Button>
+                </Link>
+            </div>
+        </div>
+    );
 };
 
-export default Navbar;
+export default NavBar;
